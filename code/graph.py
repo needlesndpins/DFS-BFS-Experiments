a1fdc17e3ff7b5a159a033a543615532bee27c2d
David-qHo <dah2217@gmail.com>
2026-02-03 22:21:21 -0500

    Finished BFS2/3 and is_connected

--- code/graph.py
+++ code/graph.py
@@ -22,8 +22,8 @@ class Graph:
             self.adj[node1].append(node2)
             self.adj[node2].append(node1)
 
-    def number_of_nodes():
-        return len()
+    def number_of_nodes(self):
+        return len(self.adj)
 
 
 #Breadth First Search
@@ -89,5 +89,152 @@ def MVC(G):
             if len(subset) < len(min_cover):
                 min_cover = subset
     return min_cover
+
+
+# * Our Code *
+
+# BFS2/DFS2 
+
+
+# Do we assume node1 != node2 ??
+def BFS2(G,node1, node2):
+
+    # Initialize parent array with -1 for every entry
+    p = [-1 for i in range(len(G.adj))] # Parent array, p[i] = cj -> vertex i is parent of vertex j
+    path = []
+
+    Q = deque([node1]) # stores vertices to visit
+    marked = {node1 : True} # Dictionary to store if vertex visited
+
+
+    # Fill up the marked dictionary with False for every vertex except node1
+    for node in G.adj:
+        if node != node1:
+            marked[node] = False
+
+
+
+    while len(Q) != 0:
+        current_node = Q.popleft() # Pop next vertex to check
+
+        for node in G.adj[current_node]: # Go through this vertice's adjacency list
+
+            if node == node2: # If connection with node2, you're done 
+                p[node] = current_node
+                n = node2 
+
+                # Go back through parent array to find path
+                while n != node1: 
+                    print(n)
+                    print(p[n])
+                    path.append(n)
+                    n = p[n]
+                path.append(node1)
+
+                path.reverse()
+                #print(f"path:{path}")
+                return path
+                
+            
+
+            if not marked[node]: # If vertex isn't marked
+                p[node] = current_node # parent of adjacent node (node) is node currently visiting (current_node)
+                Q.append(node)   # Add to deque 
+                marked[node] = True # Mark it 
+
+    return path
+
+
+# Return predecessor dictionary of form: {2 : 1, 3 : 1, 4 : 2, 5 : 3, 6 : 4}
+def BFS3(G,node1): 
+    Q = deque([node1])
+    marked = {node1 : True}
+
+    dic = {} 
+
+    for node in G.adj:
+        if node != node1:
+            marked[node] = False
+
+    while len(Q) != 0:
+
+        current_node = Q.popleft()
+
+        for node in G.adj[current_node]:
+            
+
+            if not marked[node]:
+                Q.append(node)
+                marked[node] = True 
+
+                # This check is probably not needed
+                if node not in dic: 
+                    dic[node] = current_node
+
+
+    return dic
+
+
+# Choose a vertex, and check if you visit every vertex
+# Assume all Graphs have vertex 0 (Should cause of how Graphs are defined in this file)
+def is_connected(G): 
+    num_vertices = len(G.adj)
+
+    discovered = []
+
+    Q = deque([0]) # Start at 0
+    discovered.append(0)
+
+    marked = {0 : True}
+
+    for node in G.adj:
+        if node != 0:
+            marked[node] = False
+    
+    while len(Q) != 0:
+        current_node = Q.popleft()
+
+        for node in G.adj[current_node]:
+
+            if not marked[node]:
+                discovered.append(node)
+                Q.append(node)
+                marked[node] = True
+
+
+    return len(discovered) == num_vertices
+    
+
+
+
+
+g = Graph(6)
+
+
+g.add_edge(0,1)
+g.add_edge(0,2)
+g.add_edge(1,3)
+g.add_edge(2,3)
+g.add_edge(2,4)
+g.add_edge(3,5)
+
+
+
+#BFS2(g,0,4)
+
+
+# 0 - 1 - 2    3 
+# |-------|
+
+g2 = Graph(4) 
+g2.add_edge(0,1)
+g2.add_edge(1,2)
+g2.add_edge(2,0)
+
+
+print(BFS3(g,0))
+print(is_connected(g))
+
+
 
 
